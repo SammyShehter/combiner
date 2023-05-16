7e611d9cb86cf74690e1e8214b0b6c88e3383dac
SammyShehter <sammyshehter@gmail.com>
2023-05-16 18:08:12 +0300

    init

---
+++ src/main.rs
@@ -0,0 +1,31 @@
+mod args;
+
+use args::Args;
+use image::{io::Reader, DynamicImage, ImageFormat};
+use std::{fs::File, io::BufReader};
+
+#[derive(Debug)]
+enum ImageDataErrors {
+    DifferentImageFormats,
+}
+fn main() -> Result<(), ImageDataErrors>{
+    let args = Args::new();
+    let (image1, image_format_1) = find_image_from_path(args.image_1);
+    let (image2, image_format_2) = find_image_from_path(args.image_2);
+
+    if image_format_1 != image_format_2 {
+        return Err(ImageDataErrors::DifferentImageFormats);
+    }
+
+    Ok(());
+
+    println!("{:?}", args);
+}
+
+fn find_image_from_path(path: String) -> (DynamicImage, ImageFormat) {
+    let image_reader: Reader<BufReader<File>> = Reader::open(path).unwrap();
+    let image_format: ImageFormat = image_reader.format().unwrap();
+    let image: DynamicImage = image_reader.decode().unwrap();
+
+    (image, image_format)
+}
